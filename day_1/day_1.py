158efe06294338b34f234102ef5288e53ab3d99e
JohnWocko <jon_woko@hotmail.co.uk>
2022-07-13 22:06:54 +0100

    First day of coding completed.

---
+++ day_1/day_1.py
@@ -0,0 +1,6 @@
+print('Welcome to Brand Name Genrator:\n')
+
+city = input('What is the name of your City?\n')
+pet = input('What is the name of your first pet?\n')
+
+print(f'Your brand name could be:\n {city} {pet}')
