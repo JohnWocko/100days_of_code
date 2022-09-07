f64eb7159d0ab548efc90e93d884f625beb35041
JohnWocko <jon_woko@hotmail.co.uk>
2022-09-07 20:27:52 +0100

    Restarted my 100 days. Added functions for day13.

--- day13_coffee_machine/day13_coffee_machine.py
+++ day13_coffee_machine/day13_coffee_machine.py
@@ -42,26 +42,50 @@ resources = {
     "coffee": 100,
 }
 
+def check_water(required_resources):
+    return (resources['water'] - required_resources['ingredients']['water'] ) >= 0
+
+def check_milk(required_resources):
+    return (resources['milk'] - required_resources['ingredients']['milk'] ) >= 0
+
+def check_coffee(required_resources):
+    return (resources['coffee'] - required_resources['ingredients']['coffee'] ) >= 0
+
+def check_resources(required_resources):
+    return check_coffee(required_resources) and check_milk(required_resources) and check_water(required_resources)
+
+def update_resources(required_resources):
+    if check_resources(required_resources):
+        resources['water'] -= required_resources['ingredients']['water'] 
+        resources['milk'] -= required_resources['ingredients']['milk'] 
+        resources['coffee'] -= required_resources['ingredients']['coffee']
+        return f"£{required_resources['cost']}"
+    else:
+        print('Insufficient Resources\n£0.00')
 
 def get_user_choice():
     try:
+        
         choice = input('Please select a coffee type:☕\n\n☕ - Cappuccino (c)\n☕ - Latte (l)'
         + '\n☕ - Espresso (e)\n')
 
         if choice.lower() not in 'cle':
             print('Input not recognised ...\nTry Again.')
-            get_user_choice()
+            return get_user_choice()
         elif choice.lower() == 'c':
-            return {'cappuccino': MENU['cappuccino']}
+            return ('cappuccino', MENU['cappuccino'])
         elif choice.lower() == 'l':
-            return MENU['latte']
-        elif choice.lower() == 'e': 
-            return MENU['espresso']   
-        return choice
+            return ('latte', MENU['latte'])
+        else:
+            return ('espresso', MENU['espresso'])   
+
     except:
         print('Input not recognised ...\nTry Again.')
-        print(get_user_choice())
+        return get_user_choice()
 
 
 user_choice = get_user_choice()
-print(user_choice.keys())
+print(user_choice[0],user_choice[1], update_resources(user_choice[1]))
+print(resources)
+update_resources(user_choice[1])
+print(resources)
